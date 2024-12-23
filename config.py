64a74b749fa5f0f28e6d636c1d3c00d6b1207911
ShwetaTE <123714231+ShwetaTE@users.noreply.github.com>
2024-12-23 16:10:38 +0530

    app .py changes

--- config.py
+++ config.py
@@ -11,5 +11,9 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
+    # APP_ID = os.environ.get("MicrosoftAppId", "")
+    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    # TENANT_ID= os.environ.get("MicrosoftAppTenantId", "")
     APP_ID = os.environ.get("MicrosoftAppId", "6353b651-5476-45c1-96d4-9e05e7ac150a")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "EiI8Q~3GMsAQlhBbBZT4DTS1y4gJ4iwsrYJIWaS~")
+    TENANT_ID= os.environ.get("MicrosoftAppTenantId", "3d7a3f90-1d2c-4d91-9b49-52e098cf9eb8")
